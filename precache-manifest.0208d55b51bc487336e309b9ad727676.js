48aaa32d3437967d1d6f73cab6a2ca5efead978c
Troverint <thisspecial16@gmail.com>
2023-06-01 11:58:17 +0700

    Updates

---
+++ precache-manifest.0208d55b51bc487336e309b9ad727676.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "3661ceb73f501985e76e",
+    "url": "/letrismart/static/css/main.9d3a62d2.chunk.css"
+  },
+  {
+    "revision": "3661ceb73f501985e76e",
+    "url": "/letrismart/static/js/main.3661ceb7.chunk.js"
+  },
+  {
+    "revision": "2573e8f713a9de2a1be0",
+    "url": "/letrismart/static/js/1.2573e8f7.chunk.js"
+  },
+  {
+    "revision": "32740a8dedd63551e390",
+    "url": "/letrismart/static/js/runtime~main.32740a8d.js"
+  },
+  {
+    "revision": "c35e6420ab293fb11c8cece55afc255e",
+    "url": "/letrismart/static/media/sukro.c35e6420.svg"
+  },
+  {
+    "revision": "85b072db7addb53ee87c839aa2075a07",
+    "url": "/letrismart/static/media/start.85b072db.svg"
+  },
+  {
+    "revision": "714f34c0ec221e13ad27a739a6bb0f01",
+    "url": "/letrismart/static/media/pilus.714f34c0.svg"
+  },
+  {
+    "revision": "51ef76352ad20db77fbc68dc6aa2812f",
+    "url": "/letrismart/static/media/garuda.51ef7635.svg"
+  },
+  {
+    "revision": "d605577c0960181a11133662ac66717b",
+    "url": "/letrismart/static/media/taro.d605577c.svg"
+  },
+  {
+    "revision": "3e944a125915bdb3648bf0c8f5aad58a",
+    "url": "/letrismart/static/media/momogi.3e944a12.svg"
+  },
+  {
+    "revision": "b7ce1a2d708e88be5340afbf452fc91e",
+    "url": "/letrismart/static/media/goriorio.b7ce1a2d.svg"
+  },
+  {
+    "revision": "fd6206e3a7bc0cb9adcb700196e3828f",
+    "url": "/letrismart/static/media/biskuat.fd6206e3.svg"
+  },
+  {
+    "revision": "b64aeb3132710953c9bc80e67297db1d",
+    "url": "/letrismart/static/media/fanta.b64aeb31.svg"
+  },
+  {
+    "revision": "5d1a9a117072620bd3ab5438a6ae063c",
+    "url": "/letrismart/static/media/sprite.5d1a9a11.svg"
+  },
+  {
+    "revision": "64f7de368e1e0da7e4e784f6a223eab1",
+    "url": "/letrismart/static/media/cola.64f7de36.svg"
+  },
+  {
+    "revision": "53e1c0ed322a2754bab73404a19f78c0",
+    "url": "/letrismart/static/media/milo.53e1c0ed.svg"
+  },
+  {
+    "revision": "c228e26ca14a3a1291669d9aef3ce0a6",
+    "url": "/letrismart/static/media/kopi.c228e26c.svg"
+  },
+  {
+    "revision": "396a723d57649fddf0f0b88e7d646da3",
+    "url": "/letrismart/static/media/tea.396a723d.svg"
+  },
+  {
+    "revision": "5fd17315781d31aa7cb52f4672c81bf2",
+    "url": "/letrismart/static/media/milk.5fd17315.svg"
+  },
+  {
+    "revision": "b06cfa29e62bd7a452de80b9bbbd6578",
+    "url": "/letrismart/static/media/pulpen.b06cfa29.svg"
+  },
+  {
+    "revision": "54c7e0cd2abd633d18a0110dd35e9983",
+    "url": "/letrismart/static/media/pensil.54c7e0cd.svg"
+  },
+  {
+    "revision": "da7e286c723370451268e89f3289c9f9",
+    "url": "/letrismart/static/media/buku.da7e286c.svg"
+  },
+  {
+    "revision": "5107c250f165e5a2db7f4c603c7d8523",
+    "url": "/letrismart/static/media/penghapus.5107c250.svg"
+  },
+  {
+    "revision": "6e71e824c1a84de7818cbd7bcf5c6185",
+    "url": "/letrismart/static/media/merah.6e71e824.svg"
+  },
+  {
+    "revision": "5991732c6c9bb8f608af77a3cac35afe",
+    "url": "/letrismart/static/media/rautan.5991732c.svg"
+  },
+  {
+    "revision": "7e1603e1c5c1aefb28b8225ffaf03a3b",
+    "url": "/letrismart/static/media/penggaris.7e1603e1.svg"
+  },
+  {
+    "revision": "0bc6b97e22c1e28ff01ee2bec2516c27",
+    "url": "/letrismart/static/media/ig.0bc6b97e.svg"
+  },
+  {
+    "revision": "e813faef55290aa27a208546258efc7b",
+    "url": "/letrismart/static/media/fb.e813faef.svg"
+  },
+  {
+    "revision": "d302d5778cd38ccaaacbb55b101ca9b3",
+    "url": "/letrismart/static/media/yt.d302d577.svg"
+  },
+  {
+    "revision": "fe4a66499cf565ea8d3e3f860e3bf2ad",
+    "url": "/letrismart/static/media/fb.fe4a6649.svg"
+  },
+  {
+    "revision": "f134056498f3ee0dfddf59c812c63981",
+    "url": "/letrismart/static/media/tw.f1340564.svg"
+  },
+  {
+    "revision": "50058d96c7b64c278148e9dcde7be3d6",
+    "url": "/letrismart/static/media/logo.50058d96.svg"
+  },
+  {
+    "revision": "254f4a5d733f561fc33b6d27b30ae13b",
+    "url": "/letrismart/static/media/Vector.254f4a5d.svg"
+  },
+  {
+    "revision": "d9c9f9d86403de21f439de8915a1c16c",
+    "url": "/letrismart/static/media/cart.d9c9f9d8.svg"
+  },
+  {
+    "revision": "61872fe893a86b17f0870b118b48ace5",
+    "url": "/letrismart/static/media/search.61872fe8.svg"
+  },
+  {
+    "revision": "ca784f5fc019ce7a5df4f8ac85184c7a",
+    "url": "/letrismart/static/media/carousel.ca784f5f.svg"
+  },
+  {
+    "revision": "8023b634144269e5f60b637b40fc63eb",
+    "url": "/letrismart/static/media/icon1.8023b634.svg"
+  },
+  {
+    "revision": "6015adae07ef4f4b5d5e972e2d67a219",
+    "url": "/letrismart/static/media/icon2.6015adae.svg"
+  },
+  {
+    "revision": "4f023982b1478d364227dbcb656eaa8e",
+    "url": "/letrismart/static/media/icon3.4f023982.svg"
+  },
+  {
+    "revision": "4ca1cb9668bfa597501ab97f69779c98",
+    "url": "/letrismart/static/media/icon4.4ca1cb96.svg"
+  },
+  {
+    "revision": "5161c0313b6a1a7c44fbd4d8702f514a",
+    "url": "/letrismart/static/media/category.5161c031.svg"
+  },
+  {
+    "revision": "f1944b92f15c1270f021957242b8e6e8",
+    "url": "/letrismart/index.html"
+  }
+];
